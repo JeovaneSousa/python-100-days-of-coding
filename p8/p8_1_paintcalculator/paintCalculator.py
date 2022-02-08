3593a19a86f045c4c9f105c590c055017c36a615
JeovaneSousa <jeovanesousa@icloud.com>
2022-02-08 17:55:52 -0300

    Day 8. Caeser cypher

---
+++ p8/p8_1_paintcalculator/paintCalculator.py
@@ -0,0 +1,20 @@
+from math import ceil
+#Write your code below this line 👇
+
+def paint_calc(height, width, cover):
+    n_cans = (height * width) / cover
+    n_cans = ceil(n_cans)
+    print(f"You`ll need {n_cans} cans of paint.")
+
+
+
+
+#Write your code above this line 👆
+# Define a function called paint_calc() so that the code below works.   
+
+# 🚨 Don't change the code below 👇
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
+
