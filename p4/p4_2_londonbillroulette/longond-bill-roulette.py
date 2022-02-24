2f00491067319a9feb99034cf945d45140edcd7c
Jeovane Sousa <66012358+JeovaneSousa@users.noreply.github.com>
2022-02-24 18:18:13 -0300

    Delete p4 directory

--- p4/p4_2_londonbillroulette/longond-bill-roulette.py
+++
@@ -1,7 +0,0 @@
-import random
-
-names = input("Type all the names separated by a space and a coma.\n")
-name_list = names.split(", ")
-luck = random.randint(0,len(name_list)-1)
-
-print(f"{name_list[luck]} is going to buy the meal today")
