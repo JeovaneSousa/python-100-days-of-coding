bd8f5ae9db583623ddc4a436fba46dc3433da56a
JeovaneSousa <jeovanesousa@icloud.com>
2022-05-25 13:01:29 -0300

    pandas overview

--- general-concepts/pandas-test/main.py
+++ general-concepts/pandas-test/main.py
@@ -5,30 +5,42 @@
 #             treated_line = line.strip()
 #             data.append(treated_line)
 # print(data)
-
+# ----------------------------------------------------
 
 # import csv
 # with open("weather_data.csv") as csv_data:
 #     data = csv.reader(csv_data)
+#     print(type(data))
 #     temperatures = []
 #     for line in data:
 #         print(line)
 #         if line[1] != "temp":
 #             temperatures.append(int(line[1]))
 # print(temperatures)
-
+# ----------------------------------------------------
 
 import pandas
 
+""
 data = pandas.read_csv("weather_data.csv")
+print(data)
 data_dict = data.to_dict()
+temp_list = data["temp"].to_list()
 print(data["temp"].mean())
 print(data_dict)
-temp_list = data["temp"].to_list()
-max_temp = data["temp"].max()
 print(temp_list)
-print(max_temp)
-max_temp_logical_vector = data["temp"] == max_temp
-max_temp_row = data[max_temp_logical_vector]
+# ----------------------------------------------------
+max_temp_row = data[data["temp"] == data["temp"].max()]
+print(max_temp_row)
 
-print (max_temp_row)
+# ----------------------------------------------------
+monday_data = data[data["day"] == "Monday"]
+print(monday_data)
+
+monday_temp = int(monday_data["temp"])
+print(f"The monday's temperature in C is {monday_temp}")
+monday_temp_in_fahrenheit = monday_temp * 1.8 + 32
+print(f"The monday's temperature in Fahr is {monday_temp_in_fahrenheit}")
+
+
+
