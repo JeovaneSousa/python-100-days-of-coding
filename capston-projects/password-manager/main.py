62aa6ed932aa12bdddcb0a367a210100174c7e6a
Jeovane <jeovanesousa@icloud.com>
2022-07-23 15:47:30 -0300

    password-manager finished

--- capston-projects/password-manager/main.py
+++ capston-projects/password-manager/main.py
@@ -1,27 +1,65 @@
 from tkinter import *
-
+from tkinter import messagebox
+from random import randint, choice, shuffle
+import pyperclip
 LABEL_FONT = ('arial', 14, 'bold')
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+def generate_password():
+    n_letters = randint(8, 12)
+    n_numbers = randint(2, 4)
+    n_symbols = randint(2, 4)
+
+    password_prep = []
+    [password_prep.append(choice(letters)) for _ in range(n_letters)]
+    [password_prep.append(choice(numbers)) for _ in range(n_numbers)]
+    [password_prep.append(choice(symbols)) for _ in range(n_symbols)]
+    shuffle(password_prep)
+
+    password = "".join(password_prep)
+
+    password_entry.delete(0, END)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+    messagebox.showinfo(title='Yay!', message='Password copied to Clipboard')
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-def save():
-    password_data = {'website': website_entry.get(),
-                     'email/username': email_entry.get(),
-                     'password': password_entry.get()
-                     }
-    with open("password-vault.txt", mode='a') as vault:
-        vault.write(f"{password_data.get('website')} | {password_data.get('email/username')} | {password_data.get('password')} \n\n")
+def clear():
     used_email = email_entry.get()
+    website_entry.delete(0, END)
+    email_entry.delete(0, END)
+    email_entry.insert(0, used_email)
+    password_entry.delete(0, END)
+    website_entry.focus()
 
-    def clear():
-        website_entry.delete(0, END)
-        email_entry.delete(0, END)
-        email_entry.insert(0, used_email)
-        password_entry.delete(0, END)
-        website_entry.focus()
 
-    clear()
+def save():
+    website = website_entry.get()
+    username = email_entry.get()
+    password = password_entry.get()
+
+    if len(password) == 0 or len(website) == 0:
+        messagebox.showwarning(title='ooops', message="Please, fill in all the fields correctly")
+
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f'Username: {username}\n '
+                                                              f'Password: {password}\n Is it ok to save?')
+        if is_ok:
+            with open("password-vault.txt", mode='a') as file:
+                file.write(
+                    f"{website} | {username} | {password} \n\n")
+
+            clear()
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title('Password Generator')
@@ -56,14 +94,10 @@ password_entry = Entry(width=21, bg='white', fg='black')
 password_entry.grid(row=3, column=1)
 
 # Buttons -----------------------------------
-password_generation_button = Button(text='Generate password', highlightthickness=0, fg='black', bg='white')
+password_generation_button = Button(text='Generate password', highlightthickness=0, fg='black', bg='white', command=generate_password)
 password_generation_button.grid(row=3, column=2)
 
 add_password_button = Button(text='Add password', width=36, highlightthickness=0, fg='black', bg='white', command=save)
 add_password_button.grid(row=4, column=1, columnspan=2)
-
-
-
-
 
 window.mainloop()
